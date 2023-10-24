8760b342f73efad759f3c1855970113c4ae390fa
Andxyz <anderson_x123@hotmail.com>
2023-10-24 17:16:13 -0300

    Raspberry Bluetooth test

---
+++ tests/raspberrypi3b-bluetooth/berry_bluetooth_test.py
@@ -0,0 +1,62 @@
+import bluetooth
+
+
+# https://github.com/pybluez/pybluez
+# https://pybluez.readthedocs.io/en/latest/api/index.html
+
+
+# loop para esperar a conexão de um dispositivo bluetooth
+while True:
+    device_list = bluetooth.discover_devices(
+        duration = 5,
+        lookup_names = True
+    )
+
+    for i in range(50):
+        print('\n')
+
+    if not device_list:
+        print("No devices found")
+
+    print("Found {} devices".format(len(device_list)))
+    for address, name in device_list:
+        print("  {} - {}".format(address, name))
+    break
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+bluetooth.advertise_service(
+    server_sock,
+    "SampleServer",
+    service_id = uuid,
+    service_classes = [uuid, bluetooth.SERIAL_PORT_CLASS],
+    profiles = [bluetooth.SERIAL_PORT_PROFILE],
+    # protocols=[bluetooth.OBEX_UUID]
+)
+
+print("Waiting for connection on RFCOMM channel", port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        print("Received", data)
+except OSError:
+    pass
+
+print("Disconnected.")
+
+client_sock.close()
+server_sock.close()
+print("All done.")
+
