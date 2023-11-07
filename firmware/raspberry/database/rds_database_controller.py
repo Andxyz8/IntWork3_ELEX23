c03cb80c700620bf39eb8b85c319a9323b2e2d05
Andxyz8 <anderson_x123@hotmail.com>
2023-11-07 02:10:53 -0300

    Future classes archives

--- firmware/raspberry/database/rds_database_controller.py
+++ firmware/raspberry/database/rds_database_controller.py
@@ -2,6 +2,6 @@ class RouteRecordingMediator:
 
     def __init__(self):
         pass
-    
+
     def send_notification(self):
-        print("Route recording mediator defined!!")
+        print("RDS database controller defined!!")
