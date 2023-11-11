9b127ee6badb264f68513c5178d2980caa4448e8
Andxyz <anderson_x123@hotmail.com>
2023-11-11 20:13:27 -0300

    Integrating movement detection

--- firmware/raspberry/database/database_controller.py
+++ firmware/raspberry/database/database_controller.py
@@ -1,5 +1,5 @@
 from json import load as json_load
-from requests import get as requests_get
+from uuid import uuid4 as generate_uuid
 from pandas import DataFrame
 from pyrebase.pyrebase import Storage, Firebase
 from pyrebase import initialize_app as pyrebase_initialize_app
@@ -12,13 +12,9 @@ class DatabaseController:
     __cloud_storage: Storage
 
     def __init__(self) -> None:
-        if not self.__initialize_firebase():
-            raise Exception("It was not possible to initialize firebase.")
-
-        print(f"FIREBASE STORAGE CREDENTIALS: {self.__cloud_storage.credentials}")
+        self.__initialize_firebase()
 
     def __initialize_firebase(self) -> bool:
-
         # Open credentials archive stored locally in the raspberry
         with open(
             file = "./database/credentials.json",
@@ -35,6 +31,70 @@ class DatabaseController:
 
         # Instantiate the rds cloud database operator
         self.__cloud_database = RDSDatabase()
+
+        return True
+
+    def __upload_image_to_cloud_storage(self, image_unique_id: str) -> str:
+        """Upload an image to the cloud storage and return its URL.
+
+        Args:
+            image_unique_id (str): unique id of the image stored locally.
+
+        Returns:
+            str: url of the image in the cloud storage.
+        """
+        # endpoint_firebase = f"route_execution/{image_unique_id}"
+        path_image_locally =  f"./handlers/.imgs/{image_unique_id}.jpg"
+
+        self.__cloud_storage.child(
+            'route_execution'
+        ).child(image_unique_id).put(path_image_locally)
+
+        path_image_storage = f"route_execution/{image_unique_id}.jpg"
+        firebase_url_image = self.__cloud_storage.child(
+            path_image_storage
+        ).get_url()
+
+        return firebase_url_image
+
+    def __insert_camera_triggering(
+        self,
+        id_route_execution: int,
+        reason: str,
+        image_url: str
+    ) -> bool:
+        """Inserts a camera triggering at the cloud database."""
+        query_insert = f"""
+            INSERT INTO camera_triggering (
+                id_route_execution,
+                reason,
+                image_url,
+                moment
+            ) VALUES (
+                '{id_route_execution}',
+                '{reason}',
+                '{image_url}',
+                TIMESTAMP '{get_str_datetime_agora()}'
+            );
+        """
+        self.__cloud_database.execute_insert(query_insert)
+
+        return True
+
+    def __insert_alarm_triggering(self, id_route_execution: int, reason: str) -> bool:
+        """Inserts a alarm triggering at the cloud database."""
+        query_insert = f"""
+            INSERT INTO alarm_triggering (
+                id_route_execution,
+                reason,
+                moment
+            ) VALUES (
+                '{id_route_execution}',
+                '{reason}',
+                TIMESTAMP '{get_str_datetime_agora()}'
+            );
+        """
+        self.__cloud_database.execute_insert(query_insert)
 
         return True
 
@@ -122,9 +182,28 @@ class DatabaseController:
         )
         return id_route_execution
 
+    def insert_route_execution_movement_detection(
+        self,
+        id_route_execution: int,
+        image_unique_id: str,
+        step_sequence: str
+    ) -> bool:
+        firebase_image_url = self.__upload_image_to_cloud_storage(image_unique_id)
+        triggering_reason = "Movement Detection"
+        self.__insert_camera_triggering(
+            id_route_execution = id_route_execution,
+            reason = triggering_reason,
+            image_url = firebase_image_url
+        )
+        self.__insert_alarm_triggering(
+            id_route_execution = triggering_reason,
+            reason = triggering_reason
+        )
+        return True
+
     def update_route_execution_ending(self, unique_id: int) -> bool:
         """Update the ending of a route.
-        
+
         Args:
             unique_id (int): id_route_execution of the route.
         """
@@ -177,31 +256,3 @@ class DatabaseController:
         """
         df_route_steps = self.__cloud_database.execute_select(query_select)
         return df_route_steps
-
-    def insert_alarm_triggering(self) -> bool:
-        """Inserts a alarm triggering at the cloud database."""
-        raise NotImplementedError
-
-    def insert_camera_triggering(
-        self,
-        id_route_execution: int,
-        reason: str,
-        image_url: str
-    ) -> bool:
-        """Inserts a camera triggering at the cloud database."""
-        query_insert = f"""
-            INSERT INTO camera_triggering (
-                id_route_execution,
-                reason,
-                image_url,
-                moment
-            ) VALUES (
-                '{id_route_execution}',
-                '{reason}',
-                '{image_url}',
-                TIMESTAMP '{get_str_datetime_agora()}'
-            );
-        """
-        self.__cloud_database.execute_insert(query_insert)
-
-        return True
